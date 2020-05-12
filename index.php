4cdb4effeb390182de52d39b79ecca93d0e4fcfa
roguitar88 <rogeriobsoares5@gmail.com>
2020-05-12 11:49:16 -0300

    third

--- index.php
+++ index.php
@@ -1,54 +1,38 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
+ 
 include "valida_cpf_cnpj/class/class-valida-cpf-cnpj.php";
 
 if(isset($_POST['enviarcpfoucnpj'])){
-  //$cpforcnpj = filter_input(INPUT_POST, 'cpfoucnpj');
-  $cpforcnpj = $_POST['cpfoucnpj'];
+	$err = array();
 
-  $err = array();
-  if(empty($cpforcnpj)){ $err[] = "Campo vazio, mano!"; }else{
-    // Cria um objeto sobre a classe
-    $cpf_cnpj = new ValidaCPFCNPJ($cpforcnpj);
+	if(!empty($_POST['cpfoucnpj'])) {
+		$cpforcnpj = $_POST['cpfoucnpj'];
+			
+		// Cria um objeto sobre a classe
+		$cpf_cnpj = new ValidaCPFCNPJ($cpforcnpj);   //'675.401.298-67'
 
-    // Opção de CPF ou CNPJ formatado no padrão
-    $formatado = $cpf_cnpj->formata();
-
-    // Verifica se o CPF ou CNPJ é válido
-    if ( $formatado ) {
-      echo $formatado; // 71.569.042/0001-96
-    } else {
-      $err[] = "*CPF ou CNPJ Inválido.";
-    }
-
-    #
-    # Opção sem formatação, apenas validação
-    #
-
-    // Cria um objeto sobre a classe
-    $cpf_cnpj = new ValidaCPFCNPJ($cpforcnpj);   //'675.401.298-67'
-
-    // Verifica se o CPF ou CNPJ é válido
-    if ( $cpf_cnpj->valida() ) {
-      echo 'CPF ou CNPJ válido'; // Retornará este valor
-    } else {
-      $err[] = "*CPF ou CNPJ Inválido.";
-    }
-  }
-
-  if(!$err){
-	echo "Tudo certo!";
-  }else{
-    echo '<script>alert("';
-    foreach($err as $value){
-      echo $value."\n";
-    }
-    echo '");</script>';
-  }
+		// Verifica se o CPF ou CNPJ é válido
+		if ( $cpf_cnpj->valida() ){
+		  echo 'CPF ou CNPJ válido<br/>'; // Retornará este valor
+		}else{
+		  $err[] = "*CPF ou CNPJ Inválido.";
+		}	 
+	}else{
+		$err[] = "Campo vazio, mano bro.";
+	}
+	
+	if(!$err) {
+		echo "Tudo certo!<br/>";
+	} else{
+		echo '<script>alert("';
+		foreach($err as $value){
+		  echo $value.'\n';
+		}
+		echo '");</script>';
+	}
 }
-
 ?>
 <form enctype="multipart/form-data" method="post" action="">
   <input type="text" placeholder="CPF ou CNPJ aqui" name="cpfoucnpj" />
